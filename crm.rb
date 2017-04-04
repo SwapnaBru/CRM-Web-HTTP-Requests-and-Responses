76710450a08e2edd114c39a919a1cd6a2061c440
Swapna Brungi <swapna.brungi@gmail.com>
2017-04-04 13:21:18 -0400

    created params

--- crm.rb
+++ crm.rb
@@ -1,9 +1,9 @@
 require_relative 'contact'
 require 'sinatra'
 
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
@@ -11,6 +11,10 @@ get '/' do
 end
 
 get '/contacts/new' do
+  erb :new_contact
+end
 
-
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
